0cef65fc6e9814875226309269a962813b1a1e1b
rodrush <rodneynyabiba@gmail.com>
2023-01-03 14:13:36 +0300

    Calc Average

---
+++ calcAverage.js
@@ -0,0 +1,11 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+const findAverage = (array) => {
+	if (array.length == 0) {
+		return 0;
+	} else {
+		return array.reduce((a, b) => a + b, 0) / array.length;
+	}
+};
+console.log(findAverage([]));
