ed4f27860fbde81232c5cef467d699c85e225dc9
rodrush <rodneynyabiba@gmail.com>
2023-01-17 10:13:15 +0300

    update

--- sumOfAllPositiveInArray.js
+++ sumOfAllPositiveInArray.js
@@ -1,10 +1,11 @@
-let arr = [2, 0, -14, 32, -9];
+// let arr = [2, 0, -14, 32, -9];
+let arr = [1, 2, 3, 4, 5];
 
 const lp = () => {
 	let total = 0;
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] > 0) {
-			total += i;
+			total += arr[i];
 		}
 	}
 	console.log(total);
